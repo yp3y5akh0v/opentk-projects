95be68284694a64f96e226026687090f2b74c6f5
Yuriy Peysakhov <yuriy.peysakhov@gmail.com>
2019-07-22 06:59:38 -0400

    fixed world matrix

--- VariousProjects/SharedLib/Transformation.cs
+++ VariousProjects/SharedLib/Transformation.cs
@@ -8,11 +8,11 @@ namespace SharedLib
         public static Matrix4 GetWorldMatrix(Vector3 offset, Vector3 rotation, float scale)
         {
             var result = Matrix4.Identity;
-            result = Matrix4.Mult(Matrix4.CreateScale(scale), result);
-            result = Matrix4.Mult(Matrix4.CreateRotationX(rotation.X), result);        
-            result = Matrix4.Mult(Matrix4.CreateRotationZ(rotation.Z), result);
-            result = Matrix4.Mult(Matrix4.CreateRotationY(rotation.Y), result);
             result = Matrix4.Mult(Matrix4.CreateTranslation(offset), result);
+            result = Matrix4.Mult(Matrix4.CreateRotationX(rotation.X), result);
+            result = Matrix4.Mult(Matrix4.CreateRotationY(rotation.Y), result);                  
+            result = Matrix4.Mult(Matrix4.CreateRotationZ(rotation.Z), result);
+            result = Matrix4.Mult(Matrix4.CreateScale(scale), result);
             return result;
         }
 
